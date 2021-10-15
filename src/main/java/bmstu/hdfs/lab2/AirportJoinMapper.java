3c457e2e771dccda3fe6ebbe8457a33ab5c45a83
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-10-15 21:16:20 +0300

    Scripted auto-commit on change (2021-10-15 21:16:20) by gitwatch.sh

--- src/main/java/bmstu/hdfs/lab2/AirportJoinMapper.java
+++ src/main/java/bmstu/hdfs/lab2/AirportJoinMapper.java
@@ -10,15 +10,16 @@ public class AirportJoinMapper extends Mapper<LongWritable, Text, AirportIDWrita
     private static final String whitespaceRegEx = "\\s";
     private static final String emptyString = "";
     private static final String doubleQuotesRegEx = "\"";
+    private static final int datasetIndicator = 0;
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException, NumberFormatException {
         final int indexOfSeparator = value.toString().indexOf(separator);
         String airportIdCandidate = value.toString().substring(1, indexOfSeparator - 1).replaceAll(whitespaceRegEx, emptyString).replaceAll(doubleQuotesRegEx, emptyString);
-        String airportName = value.toString().substring(indexOfSeparator + 1).replaceAll(whitespaceRegEx, emptyString).replaceAll(doubleQuotesRegEx, emptyString);
+        String airportName = value.toString().substring(indexOfSeparator + 1).replaceAll(doubleQuotesRegEx, emptyString).trim();
         try {
             int airportID = Integer.parseInt(airportIdCandidate);
-            context.write(new AirportIDWritableComparable(new IntWritable(airportID), new IntWritable(0)), new Text(airportName));
+            context.write(new AirportIDWritableComparable(new IntWritable(airportID), new IntWritable(datasetIndicator)), new Text(airportName));
         } catch (NumberFormatException ignored) {}
     }
 }
