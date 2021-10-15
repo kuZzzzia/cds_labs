fea5a7ca3380378119be360279e90e763b5ef881
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-10-15 20:00:48 +0300

    Scripted auto-commit on change (2021-10-15 20:00:48) by gitwatch.sh

--- src/main/java/bmstu/hdfs/lab2/AirportIDGroupingComparator.java
+++ src/main/java/bmstu/hdfs/lab2/AirportIDGroupingComparator.java
@@ -5,6 +5,10 @@ import org.apache.hadoop.io.WritableComparator;
 
 public class AirportIDGroupingComparator extends WritableComparator {
 
+    public AirportIDGroupingComparator() {
+        super(AirportIDWritableComparable.class, true);
+    }
+
     @Override
     public int compare(WritableComparable a, WritableComparable b) {
         AirportIDWritableComparable id1 = (AirportIDWritableComparable) a;
