498f0e1a34bfce77ff156e58237a07b9b7fb427a
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-10-15 15:40:31 +0300

    Scripted auto-commit on change (2021-10-15 15:40:31) by gitwatch.sh

--- src/main/java/bmstu/hdfs/lab2/ReduceSideJoinAirportsWithFlightsApp.java
+++ src/main/java/bmstu/hdfs/lab2/ReduceSideJoinAirportsWithFlightsApp.java
@@ -6,18 +6,27 @@ import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public class ReduceSideJoinAirportsWithFlightsApp {
+public class ReduceSideJoinFlightsWithAirportsApp {
     public static void main(String[] args) throws Exception {
-        if (args.length != 2) {
-            System.err.println("Usage: ReduceSideJoinAirportsWithFlightsApp <input path> <output path>");
+        if (args.length != 3) {
+            System.err.println("Usage: ReduceSideJoinAirportsWithFlightsApp <inputFlights path> <inputAirports path> <output path>");
             System.exit(-1);
         }
         Job job = Job.getInstance();
-        job.setJarByClass(ReduceSideJoinAirportsWithFlightsApp.class);
-        job.setJobName("Reduce side join airports with flights");
+        job.setJarByClass(ReduceSideJoinFlightsWithAirportsApp.class);
+        job.setJobName("Reduce side join flights with airports");
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class); //TODO: add mapper
-        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class);
 
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        job.setPartitionerClass();
+        job.setGroupingComparatorClass();
+        job.setReducerClass();
+        job.setMapOutputKeyClass();
+
+        job.setOutputKeyClass();
+        job.setOutputValueClass();
+        job.setNumReduceTasks(2);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
