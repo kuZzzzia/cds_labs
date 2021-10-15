a5a9f13fb9f6ed188f94698ed4be36eafa0388e6
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-10-15 15:50:12 +0300

    Scripted auto-commit on change (2021-10-15 15:50:12) by gitwatch.sh

--- src/main/java/bmstu/hdfs/lab2/FlightMapper.java
+++ src/main/java/bmstu/hdfs/lab2/FlightMapper.java
@@ -2,13 +2,12 @@ package bmstu.hdfs.lab2;
 
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.Mapper;
 
 import java.io.IOException;
 
-public class FlightMapper extends Mapper<LongWritable, Text, TextPair, Text> {
+public class FlightMapper extends Mapper<LongWritable, Text, Writable, Text> {
     @Override
-    protected void map(LongWritable key, Text value, org.apache.hadoop.mapreduce.Mapper.Context) throws IOException, InterruptedException {
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 
     }
 }
