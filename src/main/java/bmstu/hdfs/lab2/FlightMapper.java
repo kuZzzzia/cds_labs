f46cd6adaa8a0d8a4760e6552dbc434ff544905c
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-10-15 15:48:25 +0300

    Scripted auto-commit on change (2021-10-15 15:48:25) by gitwatch.sh

--- src/main/java/bmstu/hdfs/lab2/FlightMapper.java
+++ src/main/java/bmstu/hdfs/lab2/FlightMapper.java
@@ -1,4 +1,12 @@
 package bmstu.hdfs.lab2;
 
-public class FlightMapper {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.Mapper;
+
+public class FlightMapper extends Mapper<LongWritable, Text, > {
+    @Override
+    protected void map(LongWritable key, Text value, org.apache.hadoop.mapreduce.Mapper.Context) {
+        
+    }
 }
