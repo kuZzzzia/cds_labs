7c8b1ae9ffdc3de0274ea9ac4845097ecad72291
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-10-15 17:16:03 +0300

    Scripted auto-commit on change (2021-10-15 17:16:03) by gitwatch.sh

--- src/main/java/bmstu/hdfs/lab2/GroupingComparatorByAirportID.java
+++ src/main/java/bmstu/hdfs/lab2/GroupingComparatorByAirportID.java
@@ -11,6 +11,8 @@ public class GroupingComparatorByAirportID extends WritableComparator {
 
     @Override
     public int compare(WritableComparable a, WritableComparable b) {
-        
+        AirportIDWritableComparable id1 = (AirportIDWritableComparable) a;
+        AirportIDWritableComparable id2 = (AirportIDWritableComparable) b;
+        return id1.compareTo(id2);
     }
 }
