1e1197805699b4d204dec62507e03f34562ce0d8
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-10-15 16:16:54 +0300

    Scripted auto-commit on change (2021-10-15 16:16:54) by gitwatch.sh

--- src/main/java/bmstu/hdfs/lab2/AirportMapper.java
+++ src/main/java/bmstu/hdfs/lab2/AirportMapper.java
@@ -1,11 +1,12 @@
 package bmstu.hdfs.lab2;
 
+import javafx.util.Pair;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 
-public class AirportMapper extends Mapper<LongWritable, Text, Writable, Text> {
+public class AirportMapper extends Mapper<LongWritable, Text, Pair<WritableComparable, Text>, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 
