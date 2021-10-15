2355d0e8656405f76811c1b89b9cc75b2d4cc457
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-10-15 15:54:00 +0300

    Scripted auto-commit on change (2021-10-15 15:54:00) by gitwatch.sh

--- src/main/java/bmstu/hdfs/lab2/AirportCodeWritableComparable.java
+++ src/main/java/bmstu/hdfs/lab2/AirportCodeWritableComparable.java
@@ -1,4 +1,24 @@
 package bmstu.hdfs.lab2;
 
-public class AirportCodeWritableComparable {
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class AirportCodeWritableComparable implements WritableComparable {
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
+    }
 }
