f95a51defbebfa269567a41e39f349b1b8f9c710
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-10-15 15:36:39 +0300

    Scripted auto-commit on change (2021-10-15 15:36:39) by gitwatch.sh

--- src/main/java/bmstu/hdfs/lab2/ReduceSideJoinAirportsWithFlightsApp.java
+++ src/main/java/bmstu/hdfs/lab2/ReduceSideJoinAirportsWithFlightsApp.java
@@ -2,6 +2,8 @@ package bmstu.hdfs.lab2;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class ReduceSideJoinAirportsWithFlightsApp {
@@ -13,7 +15,9 @@ public class ReduceSideJoinAirportsWithFlightsApp {
         Job job = Job.getInstance();
         job.setJarByClass(ReduceSideJoinAirportsWithFlightsApp.class);
         job.setJobName("Reduce side join airports with flights");
-        FileInputFormat.addInputPath(job, new Path(args[0]));
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class); //TODO: add mapper
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class);
+        
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
     }
 }
