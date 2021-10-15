d71eef4630a43421515cffd932c7cafcfd9d9cc5
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-10-15 17:24:45 +0300

    Scripted auto-commit on change (2021-10-15 17:24:45) by gitwatch.sh

--- src/main/java/bmstu/hdfs/lab2/ReducerJoin.java
+++ src/main/java/bmstu/hdfs/lab2/ReducerJoin.java
@@ -1,4 +1,9 @@
 package bmstu.hdfs.lab2;
 
-public class ReducerJoin extends Reducer<> {
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class ReducerJoin extends Reducer<AirportIDWritableComparable, Text, Text, FloatWritable> {
+    
 }
