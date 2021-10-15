a2e9d6c11088b66ec1110e09d74083509dd42c9e
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-10-15 17:53:45 +0300

    Scripted auto-commit on change (2021-10-15 17:53:45) by gitwatch.sh

--- src/main/java/bmstu/hdfs/lab2/ReduceSideJoinFlightsWithAirportsApp.java
+++ src/main/java/bmstu/hdfs/lab2/ReduceSideJoinFlightsWithAirportsApp.java
@@ -1,7 +1,7 @@
 package bmstu.hdfs.lab2;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -15,17 +15,18 @@ public class ReduceSideJoinFlightsWithAirportsApp {
         Job job = Job.getInstance();
         job.setJarByClass(ReduceSideJoinFlightsWithAirportsApp.class);
         job.setJobName("Reduce side join flights with airports");
-        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class); //TODO: add mapper
-        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class);
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightJoinMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportJoinMapper.class);
 
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
-        job.setPartitionerClass();
-        job.setGroupingComparatorClass();
-        job.setReducerClass();
-        job.setMapOutputKeyClass();
+        job.setPartitionerClass(AirportIDPartitioner.class);
+        job.setGroupingComparatorClass(AirportIDGroupingComparator.class);
+        job.setReducerClass(ReducerJoin.class);
+        job.setMapOutputKeyClass(AirportIDWritableComparable.class);
+        job.setMapOutputValueClass(Text.class);
 
-        job.setOutputKeyClass();
-        job.setOutputValueClass();
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
         job.setNumReduceTasks(2);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
