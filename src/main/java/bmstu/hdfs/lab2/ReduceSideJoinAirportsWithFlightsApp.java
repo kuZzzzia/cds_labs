f0e2bebb59b3845df61e59807f5b54a5fb6c448f
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-10-15 15:28:59 +0300

    Scripted auto-commit on change (2021-10-15 15:28:59) by gitwatch.sh

--- src/main/java/bmstu/hdfs/lab2/ReduceSideJoinAirportsWithFlightsApp.java
+++ src/main/java/bmstu/hdfs/lab2/ReduceSideJoinAirportsWithFlightsApp.java
@@ -1,3 +1,12 @@
+package bmstu.hdfs.lab2;
+import org.apache.hadoop.mapreduce.Job;
+
 public class ReduceSideJoinAirportsWithFlightsApp {
-    public static
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: ReduceSideJoinAirportsWithFlightsApp <input path> <output path>");
+            System.exit(-1);
+        }
+        Job job = Job.getInsatnce();
+    }
 }
