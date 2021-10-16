7a544d6c1ac2abc897f12924a86fe2a94f854ba4
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-10-16 15:33:06 +0300

    Scripted auto-commit on change (2021-10-16 15:33:06) by gitwatch.sh

--- src/main/java/bmstu/iu9/hdfs/lab2/FlightJoinMapper.java
+++ src/main/java/bmstu/iu9/hdfs/lab2/FlightJoinMapper.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 
 public class FlightJoinMapper extends Mapper<LongWritable, Text, AirportIDWritableComparable, Text> {
     private static final String SEPARATOR = ",";
-    private static final String INTEGER_REG_EX = "^\\d+$";
+    private static final String FIRST_STRING_PREFIX = "\"YEAR\"";
     private static final int DESTINATION_AIRPORT_ID_INDEX = 14;
     private static final int DELAY_INDEX = 17;
     private static final int DATASET_INDICATOR = 1;
@@ -15,9 +15,9 @@ public class FlightJoinMapper extends Mapper<LongWritable, Text, AirportIDWritab
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException, NumberFormatException {
         String[] values = value.toString().split(SEPARATOR);
-        String airportIDCandidate = values[DESTINATION_AIRPORT_ID_INDEX];
-        if (airportIDCandidate.matches(INTEGER_REG_EX)) {
-            int airportID = Integer.parseInt(airportIDCandidate);
+        String airportIDString = values[DESTINATION_AIRPORT_ID_INDEX];
+        if (!airportIDString.startsWith(FIRST_STRING_PREFIX)) {
+            int airportID = Integer.parseInt(airportIDString);
             String delay = values[DELAY_INDEX];
             if (delay.length() != 0) {
                 context.write(
