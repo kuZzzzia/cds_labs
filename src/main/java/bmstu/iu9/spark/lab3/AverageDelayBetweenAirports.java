b1277630f972f48e6cb32d73d737d25e93fab998
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-11-03 15:19:19 +0300

    Scripted auto-commit on change (2021-11-03 15:19:19) by gitwatch.sh

--- src/main/java/bmstu/iu9/spark/lab3/AverageDelayBetweenAirports.java
+++ src/main/java/bmstu/iu9/spark/lab3/AverageDelayBetweenAirports.java
@@ -6,11 +6,23 @@ import java.util.Map;
 
 public class AverageDelayBetweenAirports implements Serializable {
 
-    private final String  departureAirportName;
-    private final String  destinationAirportName;
+    private final String    departureAirportName;
+    private final String    destinationAirportName;
+    private final int       maxDelay;
+    private final float     percentOfCancelledFlights;
+    private final float     percentOfDelayedFlights;
 
 
     public AverageDelayBetweenAirports(Object delaysBtwAirports, Map<String, String> airportName) {
+        // get airports names from map airportName
+        // count data
+    }
 
+    @Override
+    public String toString() {
+        return departureAirportName + " -> " + destinationAirportName +
+                "\nMax delay: " + maxDelay +
+                "\n" + percentOfCancelledFlights + " flights were cancelled" +
+                "\n" + percentOfDelayedFlights + " flights were delayed";
     }
 }
