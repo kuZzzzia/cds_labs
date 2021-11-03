3a1c56854b3b6c0014721c29d7f7c76e7e0b3382
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-11-03 12:14:02 +0300

    Scripted auto-commit on change (2021-11-03 12:14:02) by gitwatch.sh

--- src/main/java/bmstu/iu9/spark/lab3/AirportAnalyzerApp.java
+++ src/main/java/bmstu/iu9/spark/lab3/AirportAnalyzerApp.java
@@ -6,11 +6,13 @@ import org.apache.spark.api.java.JavaSparkContext;
 
 public class AirportAnalyzerApp {
     private static final String SPARK_APP_NAME = "Airport analyzer";
+    private static final String HDFS_PATH_TO_FLIGHTS = "flights.csv";
 
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName(SPARK_APP_NAME);
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        JavaRDD<String> flights = sc.textFile("flights.csv");
+        JavaRDD<String> flights = sc.textFile(HDFS_PATH_TO_FLIGHTS);
+        flights = flights.filter(flight => )
     }
 }
