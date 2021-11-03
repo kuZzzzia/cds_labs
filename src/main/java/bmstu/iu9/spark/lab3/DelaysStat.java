9b237c017faf9c2cbc55002c094637aaf29a7e75
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-11-03 16:17:56 +0300

    Scripted auto-commit on change (2021-11-03 16:17:56) by gitwatch.sh

--- src/main/java/bmstu/iu9/spark/lab3/DelaysStat.java
+++ src/main/java/bmstu/iu9/spark/lab3/DelaysStat.java
@@ -17,19 +17,31 @@ public class DelaysStat implements Serializable {
 
 
     public DelaysStat(Tuple2<Tuple2<String, String>, DelaysStat> delaysBtwAirports, Map<String, String> airportName) {
-        this.departureAirportName = airportName.get(delaysBtwAirports._1._1);
-        this.destinationAirportName = airportName.get(delaysBtwAirports._1._2);
+        this.departureAirportName = airportName.get(delaysBtwAirports._1()._1());
+        this.destinationAirportName = airportName.get(delaysBtwAirports._1()._2());
 
         DelaysStat delaysStatSrc = delaysBtwAirports._2();
 
-        this.maxDelay = delaysStatSrc.maxDelay;
-        this.flightsCount = delaysStatSrc.flightsCount;
-        this.flightsDelayedCount = delaysStatSrc.flightsDelayedCount;
-        this.flightsCancelledCount = delaysStatSrc.flightsCancelledCount;
+        this.maxDelay = delaysStatSrc.getMaxDelay();
+        this.flightsCount = delaysStatSrc.getFlightsCount();
+        this.flightsDelayedCount = delaysStatSrc.getFlightsDelayedCount();
+        this.flightsCancelledCount = delaysStatSrc.getFlightsCancelledCount();
     }
-    
-    public DelaysStat(DelaysStat delaysStat) {
 
+    protected int getMaxDelay() {
+        return this.maxDelay;
+    }
+
+    protected int getFlightsCount() {
+        return this.flightsCount;
+    }
+
+    protected int getFlightsDelayedCount() {
+        return this.flightsDelayedCount;
+    }
+
+    protected int getFlightsCancelledCount() {
+        return this.flightsCancelledCount;
     }
 
     @Override
