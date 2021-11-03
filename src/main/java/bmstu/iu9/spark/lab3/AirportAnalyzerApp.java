5ba407b14ce9aaa097c467b6d485cccd92c3a6d7
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-11-03 12:07:40 +0300

    Scripted auto-commit on change (2021-11-03 12:07:40) by gitwatch.sh

--- src/main/java/bmstu/iu9/spark/lab3/AirportAnalyzerApp.java
+++ src/main/java/bmstu/iu9/spark/lab3/AirportAnalyzerApp.java
@@ -1,12 +1,13 @@
 package bmstu.iu9.spark.lab3;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
 
 public class AirportAnalyzerApp {
     private static final String SPARK_APP_NAME = "Airport analyzer";
 
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName(SPARK_APP_NAME);
-        
+        JavaSparkContext sc = new JavaSparkContext(conf);
     }
 }
