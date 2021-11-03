09dbf039cabc1a8112d7006831dd59401ebba45b
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-11-03 12:59:33 +0300

    Scripted auto-commit on change (2021-11-03 12:59:33) by gitwatch.sh

--- src/main/java/bmstu/iu9/spark/lab3/AirportAnalyzerApp.java
+++ src/main/java/bmstu/iu9/spark/lab3/AirportAnalyzerApp.java
@@ -9,6 +9,7 @@ import scala.Tuple2;
 public class AirportAnalyzerApp {
     private static final String SPARK_APP_NAME = "Airport analyzer";
     private static final String HDFS_PATH_TO_FLIGHTS = "flights.csv";
+    private static final String FLIGHT_DATA_SEPARATOR = ",";
 
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName(SPARK_APP_NAME);
@@ -17,8 +18,38 @@ public class AirportAnalyzerApp {
         JavaRDD<String> flights = sc.textFile(HDFS_PATH_TO_FLIGHTS);
         flights = flights.filter(flight -> !flight.startsWith("\""));
 
-        JavaPairRDD<Tuple2<Integer, Integer>, Float> flightsData = flights.mapToPair(flight -> )
+        JavaPairRDD<
+                Tuple2<
+                        Integer,
+                        Integer
+                        >,
+                Float
+                > flightsData = flights.mapToPair(
+                        flight ->
+                                new Tuple2<>(
+                                        new Tuple2<>(
+                                                getAirportID(flight, index),
+                                                getAirportID(flight, index)
+                                        ),
+
+                                        )
+        );
 
         //saveAsTextFile() - method
+    }
+
+    private static String[] getFlightDataBySplittingFlightString(final String flightString) {
+        return flightString.split(FLIGHT_DATA_SEPARATOR);
+    }
+
+    private static Tuple2<Integer, Integer> makePairOfDepartureAndDestinationAirportIDs(final String[] flightData, int departureIndex, int destinationIndex) {
+        return new Tuple2<>(
+                new Integer(
+                        flightData[departureIndex]
+                ),
+                new Integer(
+                        flightData[destinationIndex]
+                )
+        );
     }
 }
