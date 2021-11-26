0c32f4cfb386653eef8c836c09024920a2a1f042
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-11-26 15:19:13 +0300

    Scripted auto-commit on change (2021-11-26 15:19:13) by gitwatch.sh

--- src/main/java/bmstu/iu9/cds/lab4/akka/ActorRouter.java
+++ src/main/java/bmstu/iu9/cds/lab4/akka/ActorRouter.java
@@ -2,9 +2,11 @@ package bmstu.iu9.cds.lab4.akka;
 
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
+import akka.routing.Router;
 
 public class ActorRouter extends AbstractActor {
-    private static ActorRef
+    private final ActorRef keeper;
+    private final Router router;
 
     @Override
     public Receive createReceive() {
