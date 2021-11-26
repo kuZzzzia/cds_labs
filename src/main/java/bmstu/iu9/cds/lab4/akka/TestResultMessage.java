b8badfd14a09bd1d53809c13cccd55ce3630d4ab
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-11-26 13:26:25 +0300

    Scripted auto-commit on change (2021-11-26 13:26:25) by gitwatch.sh

--- src/main/java/bmstu/iu9/cds/lab4/akka/TestResultMessage.java
+++ src/main/java/bmstu/iu9/cds/lab4/akka/TestResultMessage.java
@@ -2,13 +2,24 @@ package bmstu.iu9.cds.lab4.akka;
 
 public class TestResultMessage {
 
-    private int    
+    private int    packageId;
     private String status;
     private String testName;
     private String expectedResult;
     private String receivedResult;
 
+    public TestResultMessage(int packageId, String status,
+                             String testName, String expectedResult, String receivedResult) {
+        this.packageId = packageId;
+        this.status = status;
+        this.testName = testName;
+        this.expectedResult = expectedResult;
+        this.receivedResult = receivedResult;
+    }
 
+    protected int getPackageId() {
+        return packageId;
+    }
 
 
 }
