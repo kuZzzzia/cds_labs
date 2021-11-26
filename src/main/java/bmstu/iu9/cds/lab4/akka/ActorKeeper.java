0f517cd788037632acfa58943e89c141349c4de8
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-11-26 15:31:49 +0300

    Scripted auto-commit on change (2021-11-26 15:31:49) by gitwatch.sh

--- src/main/java/bmstu/iu9/cds/lab4/akka/ActorKeeper.java
+++ src/main/java/bmstu/iu9/cds/lab4/akka/ActorKeeper.java
@@ -1,11 +1,30 @@
 package bmstu.iu9.cds.lab4.akka;
 
 import akka.actor.AbstractActor;
+import akka.actor.OneForOneStrategy;
+import akka.actor.SupervisorStrategy;
+import akka.japi.pf.DeciderBuilder;
+import scala.concurrent.duration.Duration;
 
 import java.util.*;
 
 public class ActorKeeper extends AbstractActor {
     private final Map<Integer, List<TestResult>> results = new HashMap<>();
+
+    private static final int MAX_RETRIES = 10;
+    
+    private static final SupervisorStrategy strategy =
+            new OneForOneStrategy(MAX_RETRIES,
+                    Duration.create("1 minute"),
+                    DeciderBuilder.
+                            match(ArithmeticException.class, e -> resume()).
+                            match(NullPointerException.class, e -> restart()).
+                            match(IllegalArgumentException.class, e -> stop()).
+                            matchAny(o -> escalate()).build());
+    @Override
+    public SupervisorStrategy supervisorStrategy() {
+        return strategy;
+    }
 
     @Override
     public Receive createReceive() {
