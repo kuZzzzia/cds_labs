63ddf4a835f402d8144e6dedc988d8bfd06d22dc
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-11-26 15:42:54 +0300

    Scripted auto-commit on change (2021-11-26 15:42:54) by gitwatch.sh

--- src/main/java/bmstu/iu9/cds/lab4/akka/JSTestApp.java
+++ src/main/java/bmstu/iu9/cds/lab4/akka/JSTestApp.java
@@ -1,15 +1,23 @@
 package bmstu.iu9.cds.lab4.akka;
 
+import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
+
+import java.io.IOException;
+import java.util.concurrent.CompletionStage;
 
 public class JSTestApp {
     private static final String ACTOR_SYSTEM_NAME = "js test app";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ActorSystem actorSystem = ActorSystem.create(ACTOR_SYSTEM_NAME);
         ActorRef actorRouter = actorSystem.actorOf(Props.create(ActorRouter.class));
 
@@ -17,7 +25,7 @@ public class JSTestApp {
         final ActorMaterializer materializer = ActorMaterializer.create(actorSystem);
         MainHttp instance = new MainHttp(actorSystem);
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
-                instance.createRoute(system).flow(system, materializer);
+                instance.createRoute(actorSystem).flow(actorSystem, materializer);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
                 ConnectHttp.toHost("localhost", 8080),
@@ -27,7 +35,7 @@ public class JSTestApp {
         System.in.read();
         binding
                 .thenCompose(ServerBinding::unbind)
-                .thenAccept(unbound -> system.terminate());
+                .thenAccept(unbound -> actorSystem.terminate());
 
     }
 
