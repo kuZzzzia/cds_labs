1dda103540e1d9ddee630b8137357f8673ec098e
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-11-27 00:00:15 +0300

    Scripted auto-commit on change (2021-11-27 00:00:15) by gitwatch.sh

--- src/main/java/bmstu/iu9/cds/lab4/akka/MessageStoreTestResult.java
+++ src/main/java/bmstu/iu9/cds/lab4/akka/MessageStoreTestResult.java
@@ -1,10 +1,14 @@
 package bmstu.iu9.cds.lab4.akka;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class MessageStoreTestResult {
     private final String    packageId;
     private final String    result;
 
-    public MessageStoreTestResult(String packageId, String status,
+    @JsonCreator
+    public MessageStoreTestResult(@JsonProperty("packageId") String packageId, @JsonProperty("status") String status,
                                   String testName, String expectedResult, String receivedResult) {
         this.packageId = packageId;
         this.result = "Status: " + status;
