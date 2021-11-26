f3e34c687d23d0a45fa4c561efe7a6cf4e74bbc4
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-11-26 15:19:31 +0300

    Scripted auto-commit on change (2021-11-26 15:19:31) by gitwatch.sh

--- src/main/java/bmstu/iu9/cds/lab4/akka/ActorRouter.java
+++ src/main/java/bmstu/iu9/cds/lab4/akka/ActorRouter.java
@@ -2,11 +2,25 @@ package bmstu.iu9.cds.lab4.akka;
 
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
+import akka.routing.Routee;
 import akka.routing.Router;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ActorRouter extends AbstractActor {
     private final ActorRef keeper;
     private final Router router;
+
+    {
+        List<Routee> routees = new ArrayList<Routee>();
+        for (int i = 0; i < 5; i++) {
+            ActorRef r = getContext().actorOf(Props.create(Worker.class));
+            getContext().watch(r);
+            routees.add(new ActorRefRoutee(r));
+        }
+        router = new Router(new RoundRobinRoutingLogic(), routees);
+    }
 
     @Override
     public Receive createReceive() {
