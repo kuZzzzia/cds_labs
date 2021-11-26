d8051094dc3c28bb29930abcfb043917ec749cf2
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-11-26 13:14:52 +0300

    Scripted auto-commit on change (2021-11-26 13:14:52) by gitwatch.sh

--- src/main/java/bmstu/iu9/cds/lab4/akka/ActorKeeper.java
+++ src/main/java/bmstu/iu9/cds/lab4/akka/ActorKeeper.java
@@ -3,7 +3,7 @@ package bmstu.iu9.cds.lab4.akka;
 import akka.actor.AbstractActor;
 
 public class ActorKeeper extends AbstractActor {
-    private Map<>
+    private Map<> results
 
     @Override
     public Receive createReceive() {
