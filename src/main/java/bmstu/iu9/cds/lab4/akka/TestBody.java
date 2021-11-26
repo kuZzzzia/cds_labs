ab05f15464f96766fdb49b76870b22239d191253
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-11-26 23:36:39 +0300

    Scripted auto-commit on change (2021-11-26 23:36:39) by gitwatch.sh

--- src/main/java/bmstu/iu9/cds/lab4/akka/TestBody.java
+++ src/main/java/bmstu/iu9/cds/lab4/akka/TestBody.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class TestBody {
     private final String testName;
-    private final int[] params;
+    private final Object params;
     private final String expectedResult;
 
     @JsonCreator
@@ -21,7 +21,7 @@ public class TestBody {
         return testName;
     }
 
-    protected int[] getParams() {
+    protected Object getParams() {
         return params;
     }
 
