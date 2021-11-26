6770328d68efd5b59ea3cec37fee9dd89fae8565
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-11-26 15:36:03 +0300

    Scripted auto-commit on change (2021-11-26 15:36:03) by gitwatch.sh

--- src/main/java/bmstu/iu9/cds/lab4/akka/JSTestApp.java
+++ src/main/java/bmstu/iu9/cds/lab4/akka/JSTestApp.java
@@ -1,12 +1,16 @@
 package bmstu.iu9.cds.lab4.akka;
 
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
 
 public class JSTestApp {
     private static final String ACTOR_SYSTEM_NAME = "js test app";
 
     public static void main(String[] args) {
         ActorSystem actorSystem = ActorSystem.create(ACTOR_SYSTEM_NAME);
+        ActorRef actorRouter = actorSystem.actorOf(Props.create(ActorRouter.class));
+        
     }
 
 }
