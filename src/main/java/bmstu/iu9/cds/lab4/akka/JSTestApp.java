88c7a1861d4c8271809e60794f470c781947c680
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-11-26 15:44:38 +0300

    Scripted auto-commit on change (2021-11-26 15:44:38) by gitwatch.sh

--- src/main/java/bmstu/iu9/cds/lab4/akka/JSTestApp.java
+++ src/main/java/bmstu/iu9/cds/lab4/akka/JSTestApp.java
@@ -10,6 +10,8 @@ import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+
 
 import java.io.IOException;
 import java.util.concurrent.CompletionStage;
