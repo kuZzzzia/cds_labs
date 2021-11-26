b3d3468787b2d130993e705d19a01609faa11d40
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-11-26 15:48:23 +0300

    Scripted auto-commit on change (2021-11-26 15:48:23) by gitwatch.sh

--- src/main/java/bmstu/iu9/cds/lab4/akka/JSTestApp.java
+++ src/main/java/bmstu/iu9/cds/lab4/akka/JSTestApp.java
@@ -9,6 +9,7 @@ import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.server.AllDirectives;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
@@ -16,22 +17,22 @@ import akka.stream.javadsl.Flow;
 import java.io.IOException;
 import java.util.concurrent.CompletionStage;
 
-public class JSTestApp {
+public class JSTestApp extends AllDirectives {
     private static final String ACTOR_SYSTEM_NAME = "js test app";
 
-    public static void main(String[] args) extends throws IOException {
+    public static void main(String[] args) throws IOException {
         ActorSystem actorSystem = ActorSystem.create(ACTOR_SYSTEM_NAME);
         ActorRef actorRouter = actorSystem.actorOf(Props.create(ActorRouter.class));
 
-        final Http http = Http.get(system);
-        final ActorMaterializer materializer = ActorMaterializer.create(system);
-
-        //In order to access all directives we need an instance where the routes are define.
-        HttpServerMinimalExampleTest app = new HttpServerMinimalExampleTest();
-
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = app.createRoute().flow(system, materializer);
-        final CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow,
-                ConnectHttp.toHost("localhost", 8080), materializer);
+//        final Http http = Http.get(system);
+//        final ActorMaterializer materializer = ActorMaterializer.create(system);
+//
+//        //In order to access all directives we need an instance where the routes are define.
+//        HttpServerMinimalExampleTest app = new HttpServerMinimalExampleTest();
+//
+//        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = app.createRoute().flow(system, materializer);
+//        final CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow,
+//                ConnectHttp.toHost("localhost", 8080), materializer);
 
 
         final Http http = Http.get(actorSystem);
