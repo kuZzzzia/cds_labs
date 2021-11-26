5f3fff949099a2717df602c4f32e650d33ea4f72
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-11-26 15:41:43 +0300

    Scripted auto-commit on change (2021-11-26 15:41:43) by gitwatch.sh

--- src/main/java/bmstu/iu9/cds/lab4/akka/JSTestApp.java
+++ src/main/java/bmstu/iu9/cds/lab4/akka/JSTestApp.java
@@ -3,6 +3,8 @@ package bmstu.iu9.cds.lab4.akka;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.Http;
+import akka.stream.ActorMaterializer;
 
 public class JSTestApp {
     private static final String ACTOR_SYSTEM_NAME = "js test app";
@@ -10,7 +12,23 @@ public class JSTestApp {
     public static void main(String[] args) {
         ActorSystem actorSystem = ActorSystem.create(ACTOR_SYSTEM_NAME);
         ActorRef actorRouter = actorSystem.actorOf(Props.create(ActorRouter.class));
-        
+
+        final Http http = Http.get(actorSystem);
+        final ActorMaterializer materializer = ActorMaterializer.create(actorSystem);
+        MainHttp instance = new MainHttp(actorSystem);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+                instance.createRoute(system).flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
+
     }
 
 }
