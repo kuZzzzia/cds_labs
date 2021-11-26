77e14454fad2bd7d15de309ec0a0919aa1ec5348
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-11-26 23:29:53 +0300

    Scripted auto-commit on change (2021-11-26 23:29:53) by gitwatch.sh

--- src/main/java/bmstu/iu9/cds/lab4/akka/ActorTester.java
+++ src/main/java/bmstu/iu9/cds/lab4/akka/ActorTester.java
@@ -32,7 +32,7 @@ public class ActorTester extends AbstractActor {
         ScriptEngine engine = new ScriptEngineManager().getEngineByName(SCRIPT_ENGINE_NAME);
         engine.eval(jscript);
         Invocable invocable = (Invocable) engine;
-        return invocable.invokeFunction(functionName, (Object) params).toString();
+        return invocable.invokeFunction(functionName, 2, 1).toString();
     }
 
     private MessageStoreTestResult runTest(MessageTest message) {
