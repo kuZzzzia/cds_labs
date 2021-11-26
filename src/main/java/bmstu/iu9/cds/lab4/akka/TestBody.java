482ab5e8c697f34a5b398804b7d81f8bef616b32
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-11-26 22:51:26 +0300

    Scripted auto-commit on change (2021-11-26 22:51:26) by gitwatch.sh

--- src/main/java/bmstu/iu9/cds/lab4/akka/TestBody.java
+++ src/main/java/bmstu/iu9/cds/lab4/akka/TestBody.java
@@ -5,12 +5,12 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class TestBody {
     private final String testName;
-    private final String params;
+    private final int[] params;
     private final String expectedResult;
 
     @JsonCreator
     public TestBody(@JsonProperty("testName") String testName,
-                    @JsonProperty("params") String params,
+                    @JsonProperty("params") int[] params,
                     @JsonProperty("expectedResult") String expectedResult) {
         this.expectedResult =  expectedResult;
         this.params = params;
@@ -22,7 +22,7 @@ public class TestBody {
     }
 
     protected String getParams() {
-        return params;
+        return String.join(",",params);
     }
 
     protected String getExpectedResult() {
