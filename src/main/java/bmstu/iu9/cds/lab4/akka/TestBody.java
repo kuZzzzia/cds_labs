9d02788979775aac7f6a30f46c04cf65c67957b8
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-11-26 13:57:23 +0300

    Scripted auto-commit on change (2021-11-26 13:57:23) by gitwatch.sh

--- src/main/java/bmstu/iu9/cds/lab4/akka/TestBody.java
+++ src/main/java/bmstu/iu9/cds/lab4/akka/TestBody.java
@@ -1,2 +1,8 @@
-package bmstu.iu9.cds.lab4.akka;public class TestBody {
+package bmstu.iu9.cds.lab4.akka;
+
+public class TestBody {
+    private final int packageID;
+    private final String jsScript;
+    private final 
+
 }
