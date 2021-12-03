ef07d20802943bb57718938aa4cc89ef220bc13c
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-12-03 18:30:11 +0300

    Scripted auto-commit on change (2021-12-03 18:30:11) by gitwatch.sh

--- src/main/java/bmstu/iu9/cds/lab5/akka/CacheActor.java
+++ src/main/java/bmstu/iu9/cds/lab5/akka/CacheActor.java
@@ -5,6 +5,9 @@ import akka.actor.AbstractActor;
 public class CacheActor extends AbstractActor {
     @Override
     public Receive createReceive() {
-        return
+        return receiveBuilder()
+                .match(
+                        
+                )
     }
 }
