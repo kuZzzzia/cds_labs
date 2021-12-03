f019eaff5248761262ce12c14b853d19608cde03
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-12-03 19:10:23 +0300

    Scripted auto-commit on change (2021-12-03 19:10:23) by gitwatch.sh

--- src/main/java/bmstu/iu9/cds/lab5/akka/AverageHttpResponseTimeApp.java
+++ src/main/java/bmstu/iu9/cds/lab5/akka/AverageHttpResponseTimeApp.java
@@ -38,7 +38,7 @@ public class AverageHttpResponseTimeApp {
     }
 
     private static Flow<HttpRequest, HttpResponse, NotUsed> flowRequests(Http http, ActorSystem system, ActorMaterializer materializer) {
-        return Flow.of(HttpRequest.class).map(req -> req.getUri().).mapAsync()
+        return Flow.of(HttpRequest.class).map(req -> req.).mapAsync()
     }
 
     static class MessageGetResult {
