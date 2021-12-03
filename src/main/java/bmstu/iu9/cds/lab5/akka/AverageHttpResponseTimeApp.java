786801d18f03ef8a4435a9f20b15bd004471e45d
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-12-03 23:41:13 +0300

    Scripted auto-commit on change (2021-12-03 23:41:13) by gitwatch.sh

--- src/main/java/bmstu/iu9/cds/lab5/akka/AverageHttpResponseTimeApp.java
+++ src/main/java/bmstu/iu9/cds/lab5/akka/AverageHttpResponseTimeApp.java
@@ -43,10 +43,10 @@ public class AverageHttpResponseTimeApp {
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = flowHttpRequest(system, materializer, actor);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
-                ConnectHttp.toHost("localhost", 8000),
+                ConnectHttp.toHost("localhost", 8080),
                 materializer
         );
-        System.out.println("Server online at http://localhost:8000/\nPress RETURN to stop...");
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
         System.in.read();
         binding
                 .thenCompose(ServerBinding::unbind)
