b21764664ec7d90372534eb2ea353178929a7e14
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-12-03 19:41:59 +0300

    Scripted auto-commit on change (2021-12-03 19:41:59) by gitwatch.sh

--- src/main/java/bmstu/iu9/cds/lab5/akka/ActorCache.java
+++ src/main/java/bmstu/iu9/cds/lab5/akka/ActorCache.java
@@ -2,13 +2,19 @@ package bmstu.iu9.cds.lab5.akka;
 
 import akka.actor.AbstractActor;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ActorCache extends AbstractActor {
+    private Map<String, Long> results= new HashMap<>();
+
     @Override
     public Receive createReceive() {
         return receiveBuilder()
                 .match(
                         AverageHttpResponseTimeApp.MessageGetResult.class,
                         message -> {
+                            AverageHttpResponseTimeApp.MessageGetResult.class,
 
                         }
                 )
