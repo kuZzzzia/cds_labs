d45cbc5c0edf7ac552446c829c3ffbd0dc917404
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-12-03 19:29:13 +0300

    Scripted auto-commit on change (2021-12-03 19:29:13) by gitwatch.sh

--- src/main/java/bmstu/iu9/cds/lab5/akka/AverageHttpResponseTimeApp.java
+++ src/main/java/bmstu/iu9/cds/lab5/akka/AverageHttpResponseTimeApp.java
@@ -44,7 +44,10 @@ public class AverageHttpResponseTimeApp extends AllDirectives {
 
     private Route createRoute(ActorRef actor) {
         return parameter("testUrl", url ->
-                parameter(StringUnmarshallers.INTEGER, "count", count -> ))
+                parameter(StringUnmarshallers.INTEGER, "count", count -> {
+                    
+                })
+        );
     }
 
     static class MessageGetResult {
