35fbaad48901dda99edbb82807c55e1edf1aa35b
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-12-10 12:38:26 +0300

    Scripted auto-commit on change (2021-12-10 12:38:26) by gitwatch.sh

--- src/main/java/bmstu/iu9/cds/lab6/zookeeper/ActorConfig.java
+++ src/main/java/bmstu/iu9/cds/lab6/zookeeper/ActorConfig.java
@@ -2,11 +2,18 @@ package bmstu.iu9.cds.lab6.zookeeper;
 
 import akka.actor.AbstractActor;
 
+import java.util.List;
+
 
 public class ActorConfig extends AbstractActor {
+    private List<String> servers;
+    
 
     @Override
     public Receive createReceive() {
-        return null;
+        return receiveBuilder()
+                .match()
+                .match()
+                .build();
     }
 }
