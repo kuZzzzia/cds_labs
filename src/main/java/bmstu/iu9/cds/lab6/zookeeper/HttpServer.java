5180f106b4a4ace7f355781383a5cd3f0b0d1ab8
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-12-10 13:11:24 +0300

    Scripted auto-commit on change (2021-12-10 13:11:24) by gitwatch.sh

--- src/main/java/bmstu/iu9/cds/lab6/zookeeper/HttpServer.java
+++ src/main/java/bmstu/iu9/cds/lab6/zookeeper/HttpServer.java
@@ -17,7 +17,7 @@ public class HttpServer {
     private static final String     COUNT_QUERY_PARAM = "count";
     private static final String     ZERO_COUNT_STRING = "0";
     private static final Duration   TIMEOUT = Duration.ofMillis(5000);
-    private static final String     URL_PATTERN = "http://localhost"
+    private static final String     URL_PATTERN = "http://localhost%s"
 
     private final Http        http;
     private final ActorRef    actorConfig;
