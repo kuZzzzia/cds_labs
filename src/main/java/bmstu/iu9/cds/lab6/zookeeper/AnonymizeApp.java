cfa67d3b854e3636f1e45044516baf1bdc9885b7
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-12-10 15:06:04 +0300

    Scripted auto-commit on change (2021-12-10 15:06:04) by gitwatch.sh

--- src/main/java/bmstu/iu9/cds/lab6/zookeeper/AnonymizeApp.java
+++ src/main/java/bmstu/iu9/cds/lab6/zookeeper/AnonymizeApp.java
@@ -32,19 +32,7 @@ public class AnonymizeApp {
 
         final HttpServer server = new HttpServer(http, actorConfig, 8080);
 
-        ZooKeeperWatcher 
-
-        ZooKeeper zoo = new ZooKeeper("127.0.0.1:2181MB", 3000, this);
-        zoo.create("/servers/s",
-                "data".getBytes(),
-                ZooDefs.Ids.OPEN_ACL_UNSAFE ,
-                CreateMode.EPHEMERAL_SEQUENTIAL
-);
-        List<String> servers = zoo.getChildren("/servers", this);
-        for (String s : servers) {
-            byte[] data = zoo.getData("/servers/" + s, false, null);
-            System.out.println("server " + s + " data=" + new String(data));
-        }
+        ZooKeeperWatcher zooKeeperWatcher = new ZooKeeperWatcher();
 
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = server.createRoute().flow(system, materializer);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
--- src/main/java/bmstu/iu9/cds/lab6/zookeeper/ZooKeeperWatcher.java
+++ src/main/java/bmstu/iu9/cds/lab6/zookeeper/ZooKeeperWatcher.java
@@ -1,5 +1,7 @@
 package bmstu.iu9.cds.lab6.zookeeper;
 
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
 
 import java.util.List;
@@ -9,7 +11,17 @@ public class ZooKeeperWatcher {
     private final ZooKeeper zooKeeper;
 
     public ZooKeeperWatcher() {
-
+        ZooKeeper zoo = new ZooKeeper("127.0.0.1:2181MB", 3000, this);
+        zoo.create("/servers/s",
+                "data".getBytes(),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE ,
+                CreateMode.EPHEMERAL_SEQUENTIAL
+        );
+        List<String> servers = zoo.getChildren("/servers", this);
+        for (String s : servers) {
+            byte[] data = zoo.getData("/servers/" + s, false, null);
+            System.out.println("server " + s + " data=" + new String(data));
+        }
     }
 
     static class MessageSendServersList {
