5b31b5ff279f1d0067637d69c6fe7a39e5ca6b23
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-12-10 12:52:31 +0300

    Scripted auto-commit on change (2021-12-10 12:52:31) by gitwatch.sh

--- src/main/java/bmstu/iu9/cds/lab6/zookeeper/HttpServer.java
+++ src/main/java/bmstu/iu9/cds/lab6/zookeeper/HttpServer.java
@@ -8,15 +8,24 @@ import static akka.actor.Nobody.path;
 import static akka.http.javadsl.server.Directives.get;
 import static akka.http.javadsl.server.Directives.parameter;
 
-public class HTTPServer {
+public class HttpServer {
+    private static final String URL_QUERY_PARAM = "url";
+    private static final String COUNT_QUERY_PARAM = "count";
     private static final String ZERO_COUNT_STRING = "0";
+
+    private Http http;
+
+
+    public HttpServer(Http http) {
+        this.http = http;
+    }
 
     private Route route (ActorRef actorConfig) {
         return route(
                 path( "", () ->
                         get(() ->
-                        parameter("url", (url) ->
-                                parameter("count", (count) -> {
+                        parameter(URL_QUERY_PARAM, (url) ->
+                                parameter(COUNT_QUERY_PARAM, (count) -> {
                                     if (count.equals(ZERO_COUNT_STRING)) {
                                         return Http.get
                                     } else {
