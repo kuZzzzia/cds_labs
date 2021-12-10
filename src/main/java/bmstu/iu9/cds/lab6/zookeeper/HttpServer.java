442a889a18d0ba85834fdb5965e2f697c83ca3c4
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-12-10 13:29:01 +0300

    Scripted auto-commit on change (2021-12-10 13:29:01) by gitwatch.sh

--- src/main/java/bmstu/iu9/cds/lab6/zookeeper/HttpServer.java
+++ src/main/java/bmstu/iu9/cds/lab6/zookeeper/HttpServer.java
@@ -33,28 +33,28 @@ public class HttpServer {
         return route(
                 path(PATH, () ->
                         route(
-                        get(() ->
-                                parameter(URL_QUERY_PARAM, url ->
-                                        parameter(COUNT_QUERY_PARAM, count -> {
-                                            if (count.equals(ZERO_COUNT_STRING)) {
-                                                return completeWithFuture(
-                                                        http.singleRequest(
-                                                                HttpRequest.create(url)
-                                                        )
-                                                );
-                                            } else {
-                                                return completeWithFuture(Patterns.ask(
-                                                        actorConfig,
-                                                        new MessageGetRandomServerUrl(serverNumber),
-                                                        TIMEOUT
-                                                ).thenCompose(resPort ->
-                                                        http.singleRequest(
-                                                                HttpRequest.create(
-                                                                        String.format(URL_PATTERN, (String)resPort, url, Integer.parseInt(count) - 1)
+                                get(
+                                        () -> parameter(URL_QUERY_PARAM, url ->
+                                                parameter(COUNT_QUERY_PARAM, count -> {
+                                                    if (count.equals(ZERO_COUNT_STRING)) {
+                                                        return completeWithFuture(
+                                                                http.singleRequest(
+                                                                        HttpRequest.create(url)
                                                                 )
-                                                        )
-                                                ));
-                                            }
+                                                        );
+                                                    } else {
+                                                        return completeWithFuture(Patterns.ask(
+                                                                actorConfig,
+                                                                new MessageGetRandomServerUrl(serverNumber),
+                                                                TIMEOUT
+                                                        ).thenCompose(resPort ->
+                                                                http.singleRequest(
+                                                                        HttpRequest.create(
+                                                                                String.format(URL_PATTERN, (String)resPort, url, Integer.parseInt(count) - 1)
+                                                                        )
+                                                                )
+                                                        ));
+                                                    }
                                 })
                         )))
 
