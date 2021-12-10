3de092e917ccf297e5046ce6edf46c49597410ca
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-12-10 14:44:01 +0300

    Scripted auto-commit on change (2021-12-10 14:44:01) by gitwatch.sh

--- src/main/java/bmstu/iu9/cds/lab6/zookeeper/AnonymizeApp.java
+++ src/main/java/bmstu/iu9/cds/lab6/zookeeper/AnonymizeApp.java
@@ -4,11 +4,15 @@ import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
+
+import java.util.concurrent.CompletionStage;
 
 public class AnonymizeApp {
 
@@ -23,7 +27,16 @@ public class AnonymizeApp {
         final HttpServer server = new HttpServer(http, actorConfig, "8080");
 
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = server.createRoute().flow(system, materializer);
-
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> actorSystem.terminate());
     }
 
 }
