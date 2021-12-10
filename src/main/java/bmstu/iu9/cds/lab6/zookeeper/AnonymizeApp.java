82f6db8869f5c8fb4e5f699917ff50da99124170
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-12-10 14:50:04 +0300

    Scripted auto-commit on change (2021-12-10 14:50:04) by gitwatch.sh

--- src/main/java/bmstu/iu9/cds/lab6/zookeeper/AnonymizeApp.java
+++ src/main/java/bmstu/iu9/cds/lab6/zookeeper/AnonymizeApp.java
@@ -11,13 +11,18 @@ import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.CompletionStage;
 
 public class AnonymizeApp {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
         System.out.println("start!");
         ActorSystem system = ActorSystem.create();
         ActorRef actorConfig = system.actorOf(Props.create(ActorConfig.class));
@@ -26,6 +31,18 @@ public class AnonymizeApp {
         final Http http = Http.get(system);
 
         final HttpServer server = new HttpServer(http, actorConfig, 8080);
+
+        ZooKeeper zoo = new ZooKeeper("127.0.0.1:2181", 3000, this);
+        zoo.create("/servers/s",
+                "data".getBytes(),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE ,
+                CreateMode.EPHEMERAL_SEQUENTIAL
+);
+        List<String> servers = zoo.getChildren("/servers", this);
+        for (String s : servers) {
+            byte[] data = zoo.getData("/servers/" + s, false, null);
+            System.out.println("server " + s + " data=" + new String(data));
+        }
 
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = server.createRoute().flow(system, materializer);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
