5cbee129c95cf04c72de7c48d15175c66fe5503f
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-12-10 13:57:30 +0300

    Scripted auto-commit on change (2021-12-10 13:57:30) by gitwatch.sh

--- src/main/java/bmstu/iu9/cds/lab6/zookeeper/AnonymizeApp.java
+++ src/main/java/bmstu/iu9/cds/lab6/zookeeper/AnonymizeApp.java
@@ -4,6 +4,7 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.http.javadsl.Http;
+import akka.stream.ActorMaterializer;
 
 public class AnonymizeApp {
 
@@ -11,9 +12,10 @@ public class AnonymizeApp {
         System.out.println("start!");
         ActorSystem system = ActorSystem.create();
         ActorRef actorConfig = system.actorOf(Props.create(ActorConfig.class));
-        
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
 
         final Http http = Http.get(system);
+        
     }
 
 }
