c3b9865d77a328bdaaffc638924aa0563be83582
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-12-10 19:03:50 +0300

    Scripted auto-commit on change (2021-12-10 19:03:50) by gitwatch.sh

--- src/main/java/bmstu/iu9/cds/lab6/zookeeper/HttpServer.java
+++ src/main/java/bmstu/iu9/cds/lab6/zookeeper/HttpServer.java
@@ -12,7 +12,7 @@ import java.time.Duration;
 import static akka.http.javadsl.server.Directives.*;
 
 public class HttpServer implements Watcher {
-    private static final String     PATH = "get";
+    private static final String     PATH = "";
     private static final String     PATH_SERVERS = "localhost:";
     private static final String     URL_QUERY_PARAM = "url";
     private static final String     COUNT_QUERY_PARAM = "count";
