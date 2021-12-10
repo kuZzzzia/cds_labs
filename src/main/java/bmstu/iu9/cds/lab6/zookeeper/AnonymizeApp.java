5ca41b93b1aed664657c8bcddb94f8b2a843a0ac
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-12-10 18:51:45 +0300

    Scripted auto-commit on change (2021-12-10 18:51:45) by gitwatch.sh

--- src/main/java/bmstu/iu9/cds/lab6/zookeeper/AnonymizeApp.java
+++ src/main/java/bmstu/iu9/cds/lab6/zookeeper/AnonymizeApp.java
@@ -11,7 +11,7 @@ import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
-import org.apache.zookeeper.zookeeper.KeeperException;
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooKeeper;
 
 import java.io.IOException;
