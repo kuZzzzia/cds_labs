8ce715a9fc31db4ce7cf2c521ffd47eab63a8b97
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-12-10 13:43:51 +0300

    Scripted auto-commit on change (2021-12-10 13:43:51) by gitwatch.sh

--- src/main/java/bmstu/iu9/cds/lab6/zookeeper/ActorConfig.java
+++ src/main/java/bmstu/iu9/cds/lab6/zookeeper/ActorConfig.java
@@ -3,18 +3,22 @@ package bmstu.iu9.cds.lab6.zookeeper;
 import akka.actor.AbstractActor;
 import akka.actor.Actor;
 
+import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 
 public class ActorConfig extends AbstractActor {
     private final List<String> servers = new ArrayList<>();
 
+    private final Random random = new SecureRandom();
+
 
     @Override
     public Receive createReceive() {
         return receiveBuilder()
-                .match(HttpServer.MessageGetRandomServerUrl.class, msg -> sender().tell(), Actor.noSender())
+                .match(HttpServer.MessageGetRandomServerUrl.class, msg -> sender().tell(servers.get(random.nextInt(servers.size()))), Actor.noSender())
                 .match()
                 .build();
     }
