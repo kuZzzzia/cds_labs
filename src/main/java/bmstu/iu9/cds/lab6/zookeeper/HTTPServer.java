674f5ed8bd4b1a08272df0789088f706b587d475
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-12-10 12:46:34 +0300

    Scripted auto-commit on change (2021-12-10 12:46:34) by gitwatch.sh

--- src/main/java/bmstu/iu9/cds/lab6/zookeeper/HTTPServer.java
+++ src/main/java/bmstu/iu9/cds/lab6/zookeeper/HTTPServer.java
@@ -2,10 +2,25 @@ package bmstu.iu9.cds.lab6.zookeeper;
 
 import akka.http.javadsl.server.Route;
 
+import static akka.actor.Nobody.path;
+import static akka.http.javadsl.server.Directives.get;
+import static akka.http.javadsl.server.Directives.parameter;
+
 public class HTTPServer {
 
     private Route route () {
-        return route("/");
+        return route(
+                path( "", () ->
+                        get(() ->
+                        parameter("url", (url) ->
+                                parameter("count", (count) -> {
+                                    
+                                })
+                                )
+                        )
+
+                )
+        );
     }
 
 }
